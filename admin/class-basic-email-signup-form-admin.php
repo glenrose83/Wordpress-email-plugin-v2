6f531bfada5bff7b6c51a9add0c0f635f7d500cf
Glen Rose <glenrose83@gmail.com>
2025-08-20 12:13:47 +0200

    Overview and tables being created

--- admin/class-basic-email-signup-form-admin.php
+++ admin/class-basic-email-signup-form-admin.php
@@ -36,7 +36,68 @@ class Basic_Email_Signup_Form_Admin {
 	}
 
 	public function get_overview (){
-		echo 'get_overview';
+		echo "
+		<div class='overview_flex_container_white'>
+			<div class='page_title'>Welcome...</div>
+			<div class='box-container'>
+			<p class='overview_warning_text'>You currently have no signup forms... :-( </p>
+
+			<p class='overview_text'>Start getting subscribers by creating a form</p>
+
+			<p class='overview_green_text'>Click here and create a form <br/>
+				<img src='" . plugin_dir_url( dirname( __FILE__ ) ) . "assets/images/pop-up-option.svg' alt='Pop-up option'>
+
+			</p>
+
+			</div>
+		</div>
+
+		<div class='flex_container_white'>
+			<div class='page_title'>GRAPH</div>
+			<div class='box-container'></div>
+		</div>
+
+		<div class='my_flex-row'>
+
+				<div class='my_flex-box'>Content 1</div>
+				<div class='my_flex-box'>Content 2</div>
+				<div class='my_flex-box'>Content 3</div>
+
+
+
+		</div>
+
+		<div class='flex_container_white'>
+
+			<div class='flex-row'>
+
+				<div class='flex-box'>
+					<div class='page_title'>Help videos</div>
+					<div>
+					Video block
+					</div>
+				</div>
+
+				<div class='flex-box'>
+					<div class='page_title'>New videos</div>
+					<div>
+					Video block
+					</div>
+				</div>
+
+			</div>
+
+			
+		</div>
+		
+
+		
+		
+		
+		
+		
+		
+		";
 	}
 	
 	public function get_create_a_form (){
@@ -71,6 +132,27 @@ class Basic_Email_Signup_Form_Admin {
 							<td>Data 4</td>
 							<td>Data 5</td>
 						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+							<td>Data 5</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+							<td>Data 5</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+							<td>Data 5</td>
+						</tr>
 					</tbody>
 				</table>
 				
@@ -84,11 +166,116 @@ class Basic_Email_Signup_Form_Admin {
 	}
 
 	public function get_subscribers (){
-		echo 'get_subscribers';
+		echo "
+		<div class='flex_container_white'>
+			<div class='page_title'>Subscribers</div>
+			<div class='drop'>
+				Select a list<br />
+				<select name='form_type' class='regular-text'>
+					<option value=''>Choose form type</option>
+					<option value='popup'>Pop-up Form</option>
+					<option value='inline'>Inline Form</option>
+					<option value='sidebar'>Sidebar Widget</option>
+				</select>
+			</div>
+			<div class='container_table_forms'>
+				<table>
+					<thead>
+						<tr>
+							<th class='col-25'>Name</th>
+							<th class='col-25'>Email</th>
+							<th class='col-25'>List</th>
+							<th class='col-15'>Date Joined</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+						</tr>
+					</tbody>
+				</table>
+				
+			</div>
+
+
+		</div>
+		
+		";
 	}
 
 		public function get_lists (){
-		echo 'get_lists';
+
+		echo "	
+		<div class='flex_container_white'>
+			<div class='page_title'>Lists</div>
+			<div class='container_table_forms'>
+				<table>
+					<thead>
+						<tr>
+							<th class='col-25'>Name</th>
+							<th class='col-25'>List</th>
+							<th class='col-25'>Type</th>
+							<th class='col-15'>Activated</th>
+							<th class='col-10'>Edit</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+							<td>Data 5</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+							<td>Data 5</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+							<td>Data 5</td>
+						</tr>
+						<tr>
+							<td>Data 1</td>
+							<td>Data 2</td>
+							<td>Data 3</td>
+							<td>Data 4</td>
+							<td>Data 5</td>
+						</tr>
+					</tbody>
+				</table>
+				
+			</div>
+		</div>
+		";
+
 	}
 
 		public function get_send_a_email (){
